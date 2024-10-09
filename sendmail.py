df2a38534b35cece446bf27397cf233457cce7c0
appjedi <appjedi.net@gmail.com>
2024-10-09 15:51:14 -0700

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -12,7 +12,6 @@ conn = sqlite3.connect('data.db')
 def send_email(to, subject, body):
     user="timlinator@gmail.com"
     pw="nwhx npae szit giqk"
-    
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
