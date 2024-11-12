32a37535c18ad35899c8472c2106a3108e979f7e
Bob T <54815862+appjedi@users.noreply.github.com>
2024-11-12 03:43:58 -0800

    update repo
    update repo

--- Flask_Blog/02-Templates/flaskblog.py
+++ Flask_Blog/02-Templates/flaskblog.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for
-import MySQLdb
+import os
+from flask import Flask, flash, request, redirect, url_for,render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Users/roberttimlin/Documents/GitHub/python-dev/Flask_Blog/02-Templates/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+#import MySQLdb
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 posts = [
     {
@@ -23,15 +29,52 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/upload2")
+def uploadForm():
+    return render_template('upload.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 def getConn():
+    print ("getConn() called")
 #db = MySQLdb.connect(host="localhost",user="root",password="maria1",database="test")
-    db = MySQLdb.connect(host="127.0.0.1", user="root",
-                     password="Jedi2023", database="dev")
-    return db
+    #db = MySQLdb.connect(host="127.0.0.1", user="root",password="Jedi2023", database="dev")
+    return None
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5001
+    )
---
+++ mongo-test.py
@@ -0,0 +1,29 @@
+MONGO_URL="mongodb+srv://appuser:AppData2022@cluster0.aga82.mongodb.net/test"
+
+from pymongo import MongoClient
+client = MongoClient(MONGO_URL)
+
+mydatabase = client['test']
+
+mycollection=mydatabase['users']
+
+
+newUser ={
+    'username':'python3',
+    'password':'Bite1234',
+    'fullname':'Monty Python',
+    'email':'python3@example.com',
+    'birthday':'2000-01-01',
+    'role':2,
+    'favorites':[1,2,3]
+}
+id="67333dc8927931223c606614"
+filter = { 'username': 'python3' }
+ 
+# Values to be updated.
+newvalues = { "$set": { 'favorites': [1,2,3] } }
+mycollection.update_one(filter, newvalues)
+#mycollection.insert_one(newUser)
+
+for i in mycollection.find():
+    print(i)
