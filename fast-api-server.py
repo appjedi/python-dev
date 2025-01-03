269c3e22f3e45920ea5c149a2aac712590ede9a7
Bob T <54815862+appjedi@users.noreply.github.com>
2025-01-03 12:03:37 -0800

    Update fast-api-server.py

--- fast-api-server.py
+++ fast-api-server.py
@@ -47,6 +47,28 @@ async def postUser (user:User):
     resp=await query(qry, values)
     print ("resp:",resp[0])
     return {"status":200,"userId":resp[0][0],"message":resp[0][1] }
+
+@app.post("/api/auth")
+async def postUser (user:User):
+    print ("postUser called",user)
+    qry="call usp_user_auth (%s,%s)"
+    values = [user.username, user.password]
+    print ("values:",values)
+    resp=await query(qry, values)
+    print ("resp:",resp[0])
+    return {"status":200,"userId":resp[0][0],"message":resp[0][1] }
+@app.post("/api/hack")
+async def postUser (user:User): # YUCK
+    print ("postUser called",user)
+    sql= "select * from users where username='{0}' AND password='{1}'".format(user.username,user.password)
+    print ("SQL:",sql)
+    resp=await query(sql)
+    print ("resp:",resp)
+    if len(resp)>0:
+        return {"status":200,"userId":resp[0][0],"message":"success" }
+    else:
+        return {"status":401,"userId":0,"message":"invalid username or password" }
+    #return {"status":200,"userId":resp[0][0],"message":resp[0][1] }
 @app.get("/api/users")
 async def getUsers ():
     results = await query("SELECT * FROM users")
@@ -84,7 +106,7 @@ def getConn():
         host='localhost',
         user='root',
         password="Jedi2023",
-        db='dev',
+        db='training',
     )
     return connection
 
