f5c341c9e85b1cc8062dc97bf57609a08a85b704
Bob T <54815862+appjedi@users.noreply.github.com>
2025-09-26 15:22:56 -0700

    update repo

--- fast-api-server.py
+++ fast-api-server.py
@@ -28,7 +28,7 @@ myConnLive = {
     'db':'appjedin_student_temp'
 }
 
-db = MySQLConn(myConnLive)
+db = MySQLConn(myConnLocal)
 app = FastAPI()
 origins = ["*"]
 
@@ -74,7 +74,7 @@ async def postContactUs (contact:ContactUS):
     print ("resp:",resp[0]) 
     return {"status":200,"contactId":resp[0][0],"message":resp[0][2] }
 @app.post("/api/auth")
-async def postUser (user:User):
+async def authUser (user:User):
     print ("postUser called",user)
     qry="call usp_user_auth (%s,%s)"
     values = [user.username, user.password]
@@ -83,17 +83,7 @@ async def postUser (user:User):
     print ("resp:",resp[0])
     return {"status":200,"userId":resp[0][0],"message":resp[0][1] }
 
-@app.post("/api/hack")
-async def postUser (user:User): # YUCK
-    print ("postUser called",user)
-    sql= "select * from users where username='{0}' AND password='{1}'".format(user.username,user.password)
-    print ("SQL:",sql)
-    resp=await query(sql)
-    print ("resp:",resp)
-    if len(resp)>0:
-        return {"status":200,"userId":resp[0][0],"message":"success" }
-    else:
-        return {"status":401,"userId":0,"message":"invalid username or password" }
+
     #return {"status":200,"userId":resp[0][0],"message":resp[0][1] }
 @app.get("/api/users")
 async def getUsers ():
